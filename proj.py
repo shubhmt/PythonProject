b7f463411806238aa3d58da6450d52543d0af6e3
shubhmt <144799473+shubhmt@users.noreply.github.com>
2023-10-28 23:39:52 +0530

    Update proj.py

--- proj.py
+++ proj.py
@@ -29,6 +29,11 @@ class Cab:
         self.available=True
         print("Cab released")
 
+   def encrypt_password(self,password1):
+          sha256=hashlib.sha256()
+          sha256.update(password1.encode('utf-8'))
+          return sha256.hexdigest()
+
  
 
     def Employee_Login(self):
@@ -66,15 +71,49 @@ class Cab:
                     Select "pwd" from "sensitive_info_g2" where "username"=%s;
                     """
                     cur.execute(insert_query1, (usr_name,))
-                    stored_password=cur.fetchone()[0].encode('utf-8')
-                    if stored_password:
-                        if bcrypt.checkpw(entered_password.encode('utf-8'), stored_password):
-                           return True
+                
+                    stored_password=cur.fetchone()[0]
                     
-                        return False  
+                    
+
+                    usr_entered_password=self.encrypt_password(entered_password)
+
+                    
+                    print("\n")
+                    
+
+                    if usr_entered_password==stored_password:
+                         return True
+                    
+                    else:
+                         return False
                     
                     cur.close()
                     conn.commit()
+    def Update_driver_details(self):
+        #  driver_id=input("enter your driver_id:")
+         usrname=input("Enter usrname:")
+         update_query=f"update sensitive_info_g2 set pwd=%s where username=%s "
+         new_password=input("enter new password:")
+         new_password1=self.encrypt_password(new_password)
+         cur.execute(update_query,(new_password1,usrname))
+         conn.commit()
+         cur.close()
+         conn.close()
+         
+
+    def Unregistered_driver(self):
+         delete_driver_id=input("enter your driver id:")
+         usr_name=input("Enter your username:")
+         delete_query=f"DELETE FROM drivers_g2 where driver_id = {delete_driver_id};"
+         delete_query1=f"DELETE FROM sensitive_info_g2 where username = {usr_name};"
+
+
+         cur.execute(delete_query)
+         cur.execute(delete_query1)
+
+         cur.close()
+         conn.commit()
 
                     
    
@@ -87,6 +126,18 @@ class Cab:
                
                 if self.check_credentials(usr_name,entered_password):
                     print("Congratulations you logged in .......")
+
+                    print("\n 1. Update driver details")
+                    print("\n 2. Unregister_driver")
+
+                    choice=input("Enter your choice:")
+
+                    if choice=="1":
+                       self.Update_driver_details()
+                    elif choice=="2":
+                       self.Unregistered_driver()
+                    else:
+                         break;
                                 
                 else:
                     print("Password is incorrect!!! Try Again!!")
@@ -124,6 +175,9 @@ class Cab:
             password1 = input("Create your password:")
             password=self.encrypt_password(password1)
             self.registerDriver(fname,lname,usr,licence_no,driver_id,password,contact_no)
+            print("\n Congratulation you have registered succesfully!!!!!")
+            self.DriverLogin(0)
+
 
 
        
